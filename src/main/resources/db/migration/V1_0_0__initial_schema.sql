07f81b00e198d030556a6e82e1150a075f121c7c
Oto <oto.zadro@gmail.com>
2023-03-25 18:15:34 +0100

    changed initial schema script, added osoba table and normalized pacijent and skrbnik tables

--- src/main/resources/db/migration/V1_0_0__initial_schema.sql
+++ src/main/resources/db/migration/V1_0_0__initial_schema.sql
@@ -1,26 +1,27 @@
-CREATE SCHEMA IF NOT EXISTS medick;
+CREATE SCHEMA IF NOT EXISTS medickSchema;
+
+CREATE TABLE if not exists Osoba (
+    IDOsoba serial primary key  ,
+    Ime varchar(255) NOT NULL ,
+    Prezime varchar(255) NOT NULL ,
+    Email varchar(255) NOT NULL ,
+    Telefon varchar(255) NOT NULL ,
+    AdresaStanovanja varchar(255) not null ,
+    Lozinka varchar(255) not null
+
+);
+
 
 CREATE TABLE if not exists Pacijent (
      IDPacijent serial primary key  ,
-     Ime varchar(255) NOT NULL ,
-     Prezime varchar(255) NOT NULL ,
-     Email varchar(255) NOT NULL ,
-     Telefon varchar(255) NOT NULL ,
-     AdresaStanovanja varchar(255) not null ,
-     Lozinka varchar(255) not null
+     OsobaID int references Osoba(IDOsoba) not null
 
 );
 
 
 Create table if not exists Skrbnik (
-
      IDSkrbnik serial primary key,
-     Ime varchar(255) NOT NULL ,
-     Prezime varchar(255) NOT NULL ,
-     Email varchar(255) NOT NULL ,
-     Telefon varchar(255) NOT NULL ,
-     AdresaStanovanja varchar(255) not null ,
-     Lozinka varchar(255) not null
+     OsobaID int references Osoba(IDOsoba) not null
 );
 
 Create table if not exists SkrbnikPacijent(
