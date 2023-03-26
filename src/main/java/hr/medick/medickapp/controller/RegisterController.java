227b1a959e670af00bd2886861f877fda0f6862a
PetarHarambasa <croman.3365@gmail.com>
2023-03-26 13:16:24 +0200

    implemented register feature and added validation

--- src/main/java/hr/medick/medickapp/controller/RegisterController.java
+++ src/main/java/hr/medick/medickapp/controller/RegisterController.java
@@ -2,18 +2,20 @@ package hr.medick.medickapp.controller;
 
 import hr.medick.medickapp.model.Osoba;
 import hr.medick.medickapp.service.OsobaService;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class RegisterController {
 
-    @Autowired
     private OsobaService osobaService;
+
+    public RegisterController(OsobaService osobaService) {
+        this.osobaService = osobaService;
+    }
 
     @GetMapping("/register")
     public String registreIndex(){
@@ -23,6 +25,9 @@ public class RegisterController {
     @PostMapping("/register")
     public String saveOsoba(@ModelAttribute("newOsoba")Osoba osoba)
     {
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        String encodedPassword = passwordEncoder.encode(osoba.getLozinka());
+        osoba.setLozinka(encodedPassword);
         osobaService.saveOsoba(osoba);
         return "register";
     }
--- src/main/java/hr/medick/medickapp/service/OsobaService.java
+++ src/main/java/hr/medick/medickapp/service/OsobaService.java
@@ -2,13 +2,15 @@ package hr.medick.medickapp.service;
 
 import hr.medick.medickapp.model.Osoba;
 import hr.medick.medickapp.repository.OsobaRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OsobaService {
-    @Autowired
     private OsobaRepository osobaRepository;
+
+    public OsobaService(OsobaRepository osobaRepository) {
+        this.osobaRepository = osobaRepository;
+    }
 
     public void saveOsoba(Osoba osoba){
         osobaRepository.save(osoba);
