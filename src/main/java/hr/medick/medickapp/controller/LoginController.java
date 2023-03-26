30a887251f561f8b18d4ff10ef4748947ef0eb87
KristianValecic <79859420+KristianValecic@users.noreply.github.com>
2023-03-26 11:26:43 +0200

    created login page, edited style for both register and login

--- src/main/java/hr/medick/medickapp/controller/LoginController.java
+++ src/main/java/hr/medick/medickapp/controller/LoginController.java
@@ -2,13 +2,12 @@ package hr.medick.medickapp.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class RegisterController {
+public class LoginController {
 
-    @GetMapping("/register")
+    @GetMapping("/login")
     public String registreIndex(){
-        return "register";
+        return "login";
     }
 }
