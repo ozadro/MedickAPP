f9df001bd3a180b3abac04ad7b48d5bcdcee63ec
Oto <oto.zadro@gmail.com>
2023-05-04 01:22:01 +0200

    fixed searchPatient, added showPatient and viewPatient

--- src/main/java/hr/medick/medickapp/controller/SearchPacijentController.java
+++ src/main/java/hr/medick/medickapp/controller/SearchPacijentController.java
@@ -2,72 +2,93 @@ package hr.medick.medickapp.controller;
 
 import hr.medick.medickapp.model.Osoba;
 import hr.medick.medickapp.model.Pacijent;
+import hr.medick.medickapp.model.Skrbnik;
+import hr.medick.medickapp.model.SkrbnikPacijent;
 import hr.medick.medickapp.service.OsobaService;
 import hr.medick.medickapp.service.PacijentService;
-import org.apache.commons.lang3.ObjectUtils;
+import hr.medick.medickapp.service.SkrbnikPacijentService;
+import hr.medick.medickapp.service.SkrbnikService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class SearchPacijentController {
-
-
     private final PacijentService pacijentService;
-
-
     private final OsobaService osobaService;
+    private final SkrbnikService skrbnikService;
+    private final SkrbnikPacijentService skrbnikPacijentService;
 
-    public SearchPacijentController(PacijentService pacijentService, OsobaService osobaService) {
+    public SearchPacijentController(PacijentService pacijentService, OsobaService osobaService, SkrbnikService skrbnikService, SkrbnikPacijentService skrbnikPacijentService) {
         this.pacijentService = pacijentService;
         this.osobaService = osobaService;
+        this.skrbnikService = skrbnikService;
+        this.skrbnikPacijentService = skrbnikPacijentService;
     }
-
 
     @GetMapping("/searchPatient.html")
     public String searchIndex(){return "searchPatient";}
 
     @PostMapping("/foundPatient.html")
     public String searchPatient(@ModelAttribute("email")String email, Model model){
+        String errorMsg = "Unijeli ste nepostojeći E-mail, pokušajte ponovno.";
         try {
             if (email.isEmpty()) {
-                List<Pacijent> pacijentList = pacijentService.getAllPacijent();
                 List<Osoba> osobaList = new ArrayList<>();
-                for (Pacijent pacijent : pacijentList) {
+                for (Pacijent pacijent : pacijentService.getAllPacijent()) {
                     osobaList.add(pacijent.getOsoba());
-                    model.addAttribute("osobaList", osobaList);
                 }
+                model.addAttribute("osobaList", osobaList);
 
             } else {
-                Osoba osoba = osobaService.getOsobaWithThatEmail(email);
-                Boolean exists = pacijentService.isPacijent(osoba.getId());
-                if (exists) {
-                    model.addAttribute("osoba", osoba);
-                    Pacijent pacijent = pacijentService.findPacijentByOsobaEmail(email);
-                    addPatient(pacijent);
+
+                if (pacijentService.isPacijent(osobaService.getOsobaWithThatEmail(email).getId())) {
+                    List<Pacijent> pacijentList = new ArrayList<>();
+                    pacijentList.add(pacijentService.findPacijentByOsobaEmail(email));
+                    List<Osoba> osobaList = new ArrayList<>();
+                    for (Pacijent pacijent : pacijentList){
+                        osobaList.add(pacijent.getOsoba());
+                        model.addAttribute("osobaList", osobaList);
+                    }
+                }else {
+                    System.out.println(errorMsg);
+                    model.addAttribute("error", errorMsg);
                 }
             }
         }
         catch (NullPointerException exception){
-            String errorMsg = "Unijeli ste neispravan E-mail, pokušajte ponovno.";
             System.out.println(errorMsg);
-            model.addAttribute("error",errorMsg);
+            model.addAttribute("error", errorMsg);
         }
     return "searchPatient";
 }
-    @PostMapping("/addPatient.html")
-    public String addPatient(Pacijent pacijent){
 
-        // dodati ce pacijenta skrbniku kada se login postavi kak se spada
 
-     return "addPatient";
+    @PostMapping("/savePatient.html")
+    public String savePatient(@RequestParam("email")String email, Model model){
+        Pacijent pacijent = pacijentService.findPacijentByOsobaEmail(email);
+        Skrbnik skrbnik = skrbnikService.getAllSkrbnik().get(0);
+        SkrbnikPacijent skrbnikPacijent = new SkrbnikPacijent();
+
+        skrbnikPacijent.setPacijent(pacijent);
+        skrbnikPacijent.setSkrbnik(skrbnik);
+
+
+        if (!skrbnikPacijentService.existsSkrbnikPacijentByPacijentId(pacijent.getId())){
+            skrbnikPacijentService.saveSkrbnikPacijent(skrbnikPacijent);
+        }else{
+            String msg = "Odabrani pacijent ima skrbnika!";
+            model.addAttribute("msg",msg);
+        }
+
+
+        return "searchPatient";
+
     }
-
-
-
 }
--- src/main/java/hr/medick/medickapp/service/SkrbnikPacijentService.java
+++ src/main/java/hr/medick/medickapp/service/SkrbnikPacijentService.java
@@ -1,8 +1,12 @@
 package hr.medick.medickapp.service;
 
+import hr.medick.medickapp.model.Pacijent;
 import hr.medick.medickapp.model.SkrbnikPacijent;
 import hr.medick.medickapp.repository.SkrbnikPacijentRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class SkrbnikPacijentService {
@@ -13,9 +17,24 @@ public class SkrbnikPacijentService {
         this.skrbnikPacijentRepository = skrbnikPacijentRepository;
     }
 
-
     public void saveSkrbnikPacijent(SkrbnikPacijent skrbnikPacijent){
         skrbnikPacijentRepository.save(skrbnikPacijent);
+    }
+
+    public List<Pacijent> getAllPacijentsForSkrbnik(Long id){
+        List<Pacijent> pacijentList = new ArrayList<>();
+        List<SkrbnikPacijent> skrbnikPacijentList = skrbnikPacijentRepository.findSkrbnikPacijentsBySkrbnikId(id);
+
+        for (SkrbnikPacijent skrbnikPacijent : skrbnikPacijentList) {
+            pacijentList.add(skrbnikPacijent.getPacijent());
+        }
+
+
+        return pacijentList;
+    }
+
+    public Boolean existsSkrbnikPacijentByPacijentId(Long id){
+        return skrbnikPacijentRepository.existsSkrbnikPacijentByPacijentId(id);
     }
 
 
