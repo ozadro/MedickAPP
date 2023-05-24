98a5628eaec554260a15ee4b074ed668857ea848
Oto <oto.zadro@gmail.com>
2023-05-24 15:20:30 +0200

    fixed editSkrbnik so you can also change ur email

--- src/main/java/hr/medick/medickapp/controller/LoginController.java
+++ src/main/java/hr/medick/medickapp/controller/LoginController.java
@@ -1,17 +1,21 @@
 package hr.medick.medickapp.controller;
 
 import hr.medick.medickapp.model.Osoba;
+import hr.medick.medickapp.model.Skrbnik;
 import hr.medick.medickapp.service.SkrbnikService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 import java.util.List;
 
 @Controller
+@SessionAttributes("email")
 public class LoginController {
 
     private final SkrbnikService skrbnikService;
@@ -25,16 +29,18 @@ public class LoginController {
     }
 
     @PostMapping("/login")
-    public String authOsoba(@RequestParam("email") String email, @RequestParam("lozinka") String lozinka, Model model){
-        List<Osoba> osobaList = skrbnikService.getAllOsobaSkrbnik();
+    public String authOsoba(@RequestParam("email") String email, @RequestParam("lozinka") String lozinka, Model model, ModelMap modelMap){
+        List<Skrbnik> skrbnikList = skrbnikService.getAllSkrbnik();
         BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
-        if (osobaList.isEmpty()) {
+        if (skrbnikList.isEmpty()) {
             model.addAttribute("ErrorMessage", "Prijva neuspješna, pogrešni podaci o prijavi!");
         } else {
-        for (Osoba trazenaOsoba : osobaList) {
+        for (Skrbnik skrbnik : skrbnikList) {
+            Osoba trazenaOsoba = skrbnik.getOsoba();
             if (trazenaOsoba.getEmail().trim().equals(email.trim()) && passwordEncoder.matches(lozinka.trim(), trazenaOsoba.getLozinka().trim())) {
                 if (skrbnikService.isSkrbnik(trazenaOsoba.getId())){
+                    modelMap.put("email",trazenaOsoba.getEmail());
                 return "redirect:/main";
                 }
             } else {
--- src/main/java/hr/medick/medickapp/controller/ShowPacijentController.java
+++ src/main/java/hr/medick/medickapp/controller/ShowPacijentController.java
@@ -2,25 +2,33 @@ package hr.medick.medickapp.controller;
 
 import hr.medick.medickapp.model.Osoba;
 import hr.medick.medickapp.model.Pacijent;
+import hr.medick.medickapp.model.Skrbnik;
 import hr.medick.medickapp.service.SkrbnikPacijentService;
+import hr.medick.medickapp.service.SkrbnikService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
+@SessionAttributes("email")
 public class ShowPacijentController {
     private final SkrbnikPacijentService skrbnikPacijentService;
-    public ShowPacijentController(SkrbnikPacijentService skrbnikPacijentService) {
+    private final SkrbnikService skrbnikService;
+    public ShowPacijentController(SkrbnikPacijentService skrbnikPacijentService, SkrbnikService skrbnikService) {
         this.skrbnikPacijentService = skrbnikPacijentService;
+        this.skrbnikService = skrbnikService;
     }
 
     @GetMapping("/showPatient")
-    public String showPatient(Model model){
+    public String showPatient(Model model, ModelMap modelMap){
         List<Osoba> osobaList = new ArrayList<>();
-        List<Pacijent> pacijentList =  skrbnikPacijentService.getAllPacijentsForSkrbnik((long)1);
+        Skrbnik skrbnik = skrbnikService.getSkrbnikByEmail((String)modelMap.get("email"));
+        List<Pacijent> pacijentList =  skrbnikPacijentService.getAllPacijentsForSkrbnik(skrbnik.getId());
         for (Pacijent pacijent : pacijentList) {
             osobaList.add(pacijent.getOsoba());
             model.addAttribute("osobaList", osobaList);
